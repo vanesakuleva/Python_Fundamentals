e5335274330b2f67da9492f01634c8cbff820ebf
Vanesa Kuleva <105634979+vanesakuleva@users.noreply.github.com>
2022-05-31 16:30:37 +0300

    Add files via upload

---
+++ 2.Data Types and Variables /Exercise/integer_operations.py
@@ -0,0 +1,11 @@
+
+first_number= int(input())
+second_number = int(input())
+third_number = int(input())
+fourth_number = int(input())
+
+first_operation = first_number + second_number
+second_operation = first_operation // third_number
+third_operation = second_operation * fourth_number
+
+print (third_operation)
