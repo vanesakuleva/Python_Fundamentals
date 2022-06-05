48aff99bd724c80b87bee6fdf0ec81586f9973cf
Vanesa Kuleva <105634979+vanesakuleva@users.noreply.github.com>
2022-06-05 18:00:09 +0300

    Add files via upload

---
+++ 3.List Basics/Exercise/invert_values.py
@@ -0,0 +1,15 @@
+numbers = [(int (i)*-1)for i in  input().split(' ')]
+print(numbers)
+
+
+
+# number = input().split(' ')
+# new_list = []
+#
+# for i in number:
+#     i =int(i)
+#     i *=(-1)
+#     new_list.append(i)
+# print(new_list)
+
+
