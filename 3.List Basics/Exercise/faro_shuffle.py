82e6ce59dd3937aa5a517c8901d854cad3894bab
Vanesa Kuleva <105634979+vanesakuleva@users.noreply.github.com>
2022-06-07 22:09:35 +0300

    Update faro_shuffle.py

--- 3.List Basics/Exercise/faro_shuffle.py
+++ 3.List Basics/Exercise/faro_shuffle.py
@@ -1,7 +1,13 @@
-deck_of_cards = input().split(',')
+deck_of_cards = input().split()
 shuffles = int(input())
 
 for cards in range(shuffles):
     shuffled_deck = []
     middle = len(deck_of_cards) // 2
-    
+    left_deck = deck_of_cards[0:middle]
+    right_deck = deck_of_cards[middle::]
+    for index_of_the_card in range(len(left_deck)):
+        shuffled_deck.append(left_deck[index_of_the_card])
+        shuffled_deck.append(right_deck[index_of_the_card])
+    deck_of_cards = shuffled_deck
+print(deck_of_cards)
